dbd318787e0bd4de631947f9ca675bab556815bb
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-11-17 18:11:17 +0700

    commit

--- php/db.php
+++ php/db.php
@@ -1,7 +1,2 @@
 <?php
 $c = new mysqli("localhost", "prakuliah", "HelloWorld@123", "prakuliah");
-if ($c->connect_error) {
-	echo "Connection failed: " . $conn->connect_error;
-} else {
-	echo "Connected successfully";
-}
