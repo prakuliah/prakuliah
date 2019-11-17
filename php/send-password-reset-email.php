85fe9ff6b5a22893e232798c2f2d1b663908a45e
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-11-17 17:46:19 +0700

    commit

--- php/send-password-reset-email.php
+++ php/send-password-reset-email.php
@@ -1,17 +1,3 @@
 <?php
-include 'db.php';
 include 'mail.php';
-include 'uuid.php';
-include 'common.php';
-$email = $_GET["email"];
-$results = $c->query("SELECT * FROM users WHERE email='" . $email . "'");
-if (!$results || $results->num_rows <=0) {
-	// Email not registered
-	echo -1;
-	return;
-}
-$row = $results->fetch_assoc();
-$name = $row["first_name"] . " " . $row["last_name"];
-$resetID = "" . time() . "-" . generateUUID();
-$c->query("UPDATE users SET email_reset_id='" . $resetID . "' WHERE id=" . $row["id"]);
-sendEmail('danaoscompany@gmail.com', 'Atur Ulang Kata Sandi', "Hello world");
+sendEmail('danaoscompany@gmail.com', 'Atur Ulang Kata Sandi', 'Hello world');
